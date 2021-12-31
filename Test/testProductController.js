f4355d3d0978d59dede247b3ae8984f4cc5158c3
unknown <baohoang040200@gmail.com>
2021-12-31 18:04:01 +0700

    add add to cart sample test case by Hoang 31/12

--- Test/testProductController.js
+++ Test/testProductController.js
@@ -4,43 +4,43 @@ const server = require('../app');
 chai.use(chaiHttp);
 chai.should();
 
-describe('Test Suit main', () => {
-    it("It should get right product", (done) => {
-        chai.request("http://localhost:3000")
-            .get("/product/60929e4d6ce96574b4508dc8")
-            .end((err,res) => {
-                res.should.have.status(200);
-                res.text.should.include("Bàn kính Ikea Vittsjo");
-                done();
-            })
-    })
+// describe('Test Suit main', () => {
+//     it("It should get right product", (done) => {
+//         chai.request("http://localhost:3000")
+//             .get("/product/60929e4d6ce96574b4508dc8")
+//             .end((err,res) => {
+//                 res.should.have.status(200);
+//                 res.text.should.include("Bàn kính Ikea Vittsjo");
+//                 done();
+//             })
+//     })
 
 
-    it("Search", (done) => {
-        chai.request("http://localhost:3000")       
-            .get("/search")
-            .query({search:"Micke"})
-            .end((err,res) => {
-                res.text.should.include("Bàn làm việc Ikea Micke");
-                //res.text.should.include("Giường ngủ");
-                //console.log(res);
-                done();
-            })
-    })
+//     it("Search", (done) => {
+//         chai.request("http://localhost:3000")       
+//             .get("/search")
+//             .query({search:"Micke"})
+//             .end((err,res) => {
+//                 res.text.should.include("Bàn làm việc Ikea Micke");
+//                 //res.text.should.include("Giường ngủ");
+//                 //console.log(res);
+//                 done();
+//             })
+//     })
 
-    it("It should get product defaul", (done) => {
-        chai.request("http://localhost:3000")       
-            .post("/product/product-filter")
-            .send({selection:"609138204045f5b26f8c79ab"})
-            .end((err,res) => {
-                res.text.should.include("Bàn làm việc Ikea Micke");
-                //expect(res.text).to.include("Bàn làm việc Ikea")
-                //res.text.should.include("Giường ngủ");
-                //console.log(res);
-                done();
-            })
-    })
-})
+//     it("It should get product defaul", (done) => {
+//         chai.request("http://localhost:3000")       
+//             .post("/product/product-filter")
+//             .send({selection:"609138204045f5b26f8c79ab"})
+//             .end((err,res) => {
+//                 res.text.should.include("Bàn làm việc Ikea Micke");
+//                 //expect(res.text).to.include("Bàn làm việc Ikea")
+//                 //res.text.should.include("Giường ngủ");
+//                 //console.log(res);
+//                 done();
+//             })
+//     })
+// })
 
 //Minh Phat
 describe('Test suit 1', () => {
@@ -56,8 +56,8 @@ describe('Test suit 1', () => {
     })
 })
 
-//Phat
-describe('Test suit 2', () => {
+//Phat done.
+describe('Test suit get product by ID', () => {
     it(" should get right product by id", (done) => {
         chai.request("http://localhost:3000")
             .get("/product/60929e4d6ce96574b4508dc9")
